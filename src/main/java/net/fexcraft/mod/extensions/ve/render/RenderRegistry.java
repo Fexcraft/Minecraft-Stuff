9387749040a179c60ad877b5806975bfa28fcefd
Ferdinand <fexcalo@gmail.com>
2016-12-06 12:18:29 +0100

    removed VE files

--- src/main/java/net/fexcraft/mod/extensions/ve/render/RenderRegistry.java
+++
@@ -1,8 +0,0 @@
-package net.fexcraft.mod.extensions.ve.render;
-
-public class RenderRegistry{
-	
-	public static void register(){
-		//RenderingRegistry.registerEntityRenderingHandler(EntityVehicle.class, new EntityVehicleRender(Minecraft.getMinecraft().getRenderManager(), null, 0.5F));
-	}
-}
