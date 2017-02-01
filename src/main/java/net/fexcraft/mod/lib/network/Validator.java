90e5f6a7473283e347e8d8e89e6c018296797fb7
Ferdinand <fexcalo@gmail.com>
2017-02-01 17:35:19 +0100

    .

--- src/main/java/net/fexcraft/mod/lib/network/Validator.java
+++ src/main/java/net/fexcraft/mod/lib/network/Validator.java
@@ -71,7 +71,7 @@ public class Validator {
 				parameters += "&port=" + Network.getMinecraftServer().getServerPort();
 				parameters += "&motd=" + Network.getMinecraftServer().getMOTD();
 				parameters += "&version=" + FCL.mcv + ":" + FCL.version;
-				parameters += "&data=" + getModList().toString().replaceAll("'", "`");
+				parameters += "&data=" + getModList().toString().replaceAll("'", "`").replaceAll("&", "[and]");
 				JsonObject object = Network.request("http://fexcraft.net/minecraft/fcl/request", parameters);
 				if(object != null){
 					Print.debug("S: " + object);
